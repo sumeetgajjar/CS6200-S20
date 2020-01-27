128caf38b25228703b0a66b392dc4341a11dbe35
gajjar.s <gajjar.s@husky.neu.edu>
2020-01-26 19:10:58 -0500

    1. added implementation for unigram with lm jelinek mercer smoothing

--- HW_1/main.py
+++ HW_1/main.py
@@ -282,6 +282,54 @@ def find_scores_using_unigram_lm_with_laplace_smoothing(queries):
     Utils.write_results_to_file('results/unigram_lm_with_laplace_smoothing.txt', temp)
 
 
+def calculate_unigram_lm_with_jelinek_mercer_smoothing_scores(document_ids, query, vocabulary_size, lam=0.8):
+    term_vectors = EsUtils.get_termvectors(Constants.AP_DATA_INDEX_NAME, document_ids, 10000)
+    scores = []
+    for term_vector in term_vectors:
+        if term_vector['term_vectors']:
+            score = 0.0
+            valid = False
+            for token in query['tokens']:
+                if token in term_vector['term_vectors']['text']['terms']:
+                    tf = term_vector['term_vectors']['text']['terms'][token]['term_freq']
+                    ttf = term_vector['term_vectors']['text']['terms'][token]['ttf']
+                    doc_length = len(term_vector['term_vectors']['text']['terms'])
+                    temp_1 = lam * (tf / doc_length)
+                    temp_2 = (1 - lam) * (ttf / vocabulary_size)
+
+                    score += math.log(temp_1 + temp_2)
+                    valid = True
+
+            if valid:
+                scores.append((score, term_vector['_id']))
+    return scores
+
+
+@timing
+def find_scores_using_unigram_lm_with_jelinek_mercer_smoothing(queries):
+    temp = []
+    all_document_ids = EsUtils.get_all_document_ids(Constants.AP_DATA_INDEX_NAME)
+    vocabulary_size = EsUtils.get_vocabulary_size(index_name=Constants.AP_DATA_INDEX_NAME)
+    for query in queries:
+        results = Utils.run_task_parallelly(calculate_unigram_lm_with_jelinek_mercer_smoothing_scores,
+                                            all_document_ids, 8,
+                                            query=query,
+                                            vocabulary_size=vocabulary_size)
+        scores = []
+        for result in results:
+            scores.extend(result)
+        scores.sort(reverse=True)
+        for ix, score in enumerate(scores[:1000]):
+            temp.append({
+                'doc_no': score[1],
+                'rank': ix + 1,
+                'score': score[0],
+                'query_number': query['id']
+            })
+
+    Utils.write_results_to_file('results/unigram_lm_with_jelinek_mercer_smoothing.txt', temp)
+
+
 if __name__ == '__main__':
     Utils.configure_logging()
     # create_ap_data_index_and_insert_documents()
@@ -290,4 +338,5 @@ if __name__ == '__main__':
     # find_scores_using_okapi_tf(_queries)
     # find_scores_using_okapi_tf_idf(_queries)
     # find_scores_using_okapi_bm25(_queries)
-    find_scores_using_unigram_lm_with_laplace_smoothing(_queries)
+    # find_scores_using_unigram_lm_with_laplace_smoothing(_queries)
+    find_scores_using_unigram_lm_with_jelinek_mercer_smoothing(_queries)
