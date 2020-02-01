ca172fde19dbcbcfcafe937e6b50379bb8847622
gajjar.s <gajjar.s@husky.neu.edu>
2020-02-01 15:44:58 -0500

    1. added tokenizer class

---
+++ HW_2/tokenizer.py
@@ -0,0 +1,17 @@
+import re
+
+
+class Tokenzier:
+    def __init__(self) -> None:
+        self.split_regex = re.compile("(\\w+)")
+
+    def tokenize(self, document: str) -> list:
+        tokens = re.findall(self.split_regex, document)
+        i = 1
+
+        result = []
+        for token in tokens:
+            result.append((token, i))
+            i += 1
+
+        return result
