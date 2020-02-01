d7925b2540989f2ca92c71acfd05478e49fccebf
gajjar.s <gajjar.s@husky.neu.edu>
2020-02-01 17:49:07 -0500

    added more structure to the index class

--- HW_2/index.py
+++ HW_2/index.py
@@ -4,8 +4,34 @@ class CustomIndex:
         self.stopwords_filter = stopwords_filter
         self.stemmer = stemmer
 
+    @classmethod
+    def _calculate_and_update_tf_info(cls, document_id, tokens, tf_info):
+        for token in tokens:
+            term = token[0]
+            if term not in tf_info:
+                tf_info[term]['ttf'] = 0
+                tf_info[term]['tf'] = {}
+
+            term_tf_info = tf_info[term]['tf']
+            if document_id not in term:
+                term_tf_info[document_id] = {'tf': 0, 'pos': []}
+
+            termvector = term_tf_info[document_id]
+            termvector['tf'] += 1
+            termvector['pos'].append(token[1])
+
+    def _write_tf_info_to_index_file(self, tf_info):
+        catalog = {}
+        index_file_path = ''
+        return catalog, index_file_path
+
+    def _write_catalog_to_file(self, catalog):
+        catalog_file_path = ''
+        return catalog_file_path
+
     def add_documents(self, documents, index_head, enable_stemming):
 
+        tf_info = {}
         for document in documents:
             tokens = self.tokenizer.tokenize(document['text'])
 
@@ -17,3 +43,9 @@ class CustomIndex:
 
             if enable_stemming:
                 tokens = [self.stemmer.stem(token) for token in tokens]
+
+            self._calculate_and_update_tf_info(document['id'], tokens, tf_info)
+
+        catalog, index_file_path = self._write_tf_info_to_index_file(tf_info)
+        catalog_file_path = self._write_catalog_to_file(catalog)
+        return catalog_file_path, index_file_path
