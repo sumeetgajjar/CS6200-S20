68906892d7b271f79d5cde5c4bf79380576dc2fe
gajjar.s <gajjar.s@husky.neu.edu>
2020-02-20 16:54:25 -0500

    1. added implementation to get crawl delay and logging if an exception occurs 2. added logging for services 3. add method to check if the content is html

--- HW_3/crawler.py
+++ HW_3/crawler.py
@@ -1,30 +1,41 @@
+import logging
 import threading
 import time
 from datetime import datetime
 from functools import lru_cache
+from typing import Tuple
 from urllib.parse import urljoin
 from urllib.robotparser import RobotFileParser
 
-from CS6200_S20_SHARED.url_cleaner import UrlDetail
+import requests
+
+from CS6200_S20_SHARED.url_cleaner import UrlDetail, UrlCleaner
 from constants.constants import Constants
+from utils.singleton import SingletonMeta
+from utils.utils import Utils
 
 
 class UrlDistributionService:
     pass
 
 
-class CrawlingRateLimitingService:
+class CrawlingRateLimitingService(metaclass=SingletonMeta):
 
     def __init__(self) -> None:
         self.domains_being_crawled = {}
         self.lock = threading.Lock()
 
     @classmethod
-    def _get_crawl_delay(cls, rp: RobotFileParser):
-        try:
-            return int(rp.crawl_delay("*"))
-        except:
-            return Constants.DEFAULT_CRAWL_DELAY
+    def _get_crawl_delay(cls, rp: RobotFileParser) -> int:
+        crawl_delay = rp.crawl_delay("*")
+        if crawl_delay:
+            try:
+                return int(crawl_delay)
+            except:
+                logging.error("Error occurred while parsing crawl_delay: {}".format(crawl_delay), exc_info=True)
+                pass
+
+        return Constants.DEFAULT_CRAWL_DELAY
 
     def might_block(self, url_detail: UrlDetail, rp: RobotFileParser) -> None:
         domain = url_detail.domain
@@ -34,24 +45,55 @@ class CrawlingRateLimitingService:
                 crawl_delay = self._get_crawl_delay(rp)
                 secs_to_wait = crawl_delay - (datetime.now() - last_crawling_time).total_seconds()
                 if secs_to_wait > 0.0001:
+                    logging.info("Blocking {} for {} secs".format(domain, secs_to_wait))
                     time.sleep(secs_to_wait)
 
             self.domains_being_crawled[domain] = datetime.now()
 
 
-class RobotsTxtService:
+class RobotsTxtService(metaclass=SingletonMeta):
     _ROBOTS_TXT_FILE_NAME = "robots.txt"
 
-    @classmethod
     @lru_cache(maxsize=Constants.ROBOTS_TXT_CACHE_SIZE)
-    def get_robot_txt(cls, host: str) -> RobotFileParser:
+    def get_robot_txt(self, host: str) -> RobotFileParser:
+        robots_txt_url = urljoin(host, self._ROBOTS_TXT_FILE_NAME)
+        logging.info("Fetching robots.txt: {}".format(robots_txt_url))
         rp = RobotFileParser()
-        rp.set_url(urljoin(host, cls._ROBOTS_TXT_FILE_NAME))
+        rp.set_url(robots_txt_url)
         rp.read()
         return rp
 
 
 class Crawler:
 
-    def crawl(self, url_detail: UrlDetail):
-        pass
+    def __init__(self, robots_txt_service: RobotsTxtService, rate_limiter: CrawlingRateLimitingService) -> None:
+        self.robots_txt_service = robots_txt_service
+        self.rate_limiter = rate_limiter
+
+    @classmethod
+    def _is_html(cls, url_detail: UrlDetail) -> Tuple[bool, str]:
+        head_response = requests.head(url_detail.canonical_url)
+        content_type = head_response.headers.get('content-type').strip()
+        return content_type == 'text/html', content_type
+
+    def crawl(self, url_detail: UrlDetail) -> None:
+        rp = self.robots_txt_service.get_robot_txt(url_detail.host)
+        self.rate_limiter.might_block(url_detail, rp)
+
+        is_html, content_type = self._is_html(url_detail)
+        if is_html:
+            response = requests.get(url_detail.canonical_url)
+            print(response)
+        else:
+            logging.info("Dropping non html ({}) url: {}".format(content_type, url_detail.canonical_url))
+
+
+if __name__ == '__main__':
+    Utils.configure_logging()
+    a = UrlCleaner().get_canonical_url("https://docs.python.org/3/library/urllib.request.html")
+    c = Crawler(RobotsTxtService(), CrawlingRateLimitingService())
+    c.crawl(a)
+
+    a = UrlCleaner().get_canonical_url(
+        "https://user-media-prod-cdn.itsre-sumo.mozilla.net/uploads/products/2018-10-03-20-10-50-e35beb.png")
+    c.crawl(a)
