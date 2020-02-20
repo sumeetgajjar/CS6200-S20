9edd44b5b7043d394832ace041a7d8fdff376e89
gajjar.s <gajjar.s@husky.neu.edu>
2020-02-19 20:44:12 -0500

    added mysql connection creation

--- HW_3/connection_factory.py
+++ HW_3/connection_factory.py
@@ -1,3 +1,4 @@
+import mysql.connector
 import redis
 
 from utils.singleton import SingletonMeta
@@ -8,24 +9,24 @@ class RedisConnection(metaclass=SingletonMeta):
     def __init__(self, host='127.0.0.1', port=6379, socket_timeout=10) -> None:
         self.host = host
         self.port = port
-        self.client = redis.Redis(host, port, socket_timeout=socket_timeout, decode_responses=True)
+        self.conn = redis.Redis(host, port, socket_timeout=socket_timeout, decode_responses=True)
 
 
-class MysqlConnection(metaclass=SingletonMeta):
+class MysqlConnection:
 
     def __init__(self, host='127.0.0.1', port=3306) -> None:
-        pass
-
-    def get_client(self):
-        pass
+        self.host = host
+        self.port = port
+        self.conn = mysql.connector.connect(pool_size='cs6200', database='cs6200', user='cs6200', password='cs6200',
+                                            host=host, port=port)
 
 
 class ConnectionFactory:
 
     @classmethod
-    def get_redis_connection(cls) -> RedisConnection:
+    def create_redis_connection(cls) -> RedisConnection:
         return RedisConnection()
 
     @classmethod
-    def get_mysql_connection(cls) -> MysqlConnection:
+    def create_mysql_connection(cls) -> MysqlConnection:
         return MysqlConnection()
