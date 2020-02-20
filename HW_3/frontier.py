c4f0182085e2f0d8edf56afcb9759deb85427246
gajjar.s <gajjar.s@husky.neu.edu>
2020-02-19 20:29:02 -0500

    added a dummy implementation for frontier

--- HW_3/frontier.py
+++ HW_3/frontier.py
@@ -7,6 +7,13 @@ class FrontierManager:
 
     def __init__(self, seed_urls: List[UrlDetail]) -> None:
         self.seed_urls = seed_urls
+        self.queue = []
+        self.queue.extend(seed_urls)
 
     def get_urls_to_crawl(self, batch_size=20) -> List[UrlDetail]:
-        pass
+        batch = self.queue[:batch_size]
+        self.queue = self.queue[len(batch):]
+        return batch
+
+    def add_to_queue(self, urls_to_queue: List[UrlDetail]):
+        self.queue.extend(urls_to_queue)
