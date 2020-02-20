60168d2b25aee2e2b6128639b49206a2f21dca70
gajjar.s <gajjar.s@husky.neu.edu>
2020-02-20 12:52:19 -0500

    done with implementation of connection factory

--- HW_3/connection_factory.py
+++ HW_3/connection_factory.py
@@ -1,32 +1,38 @@
-import mysql.connector
+from contextlib import closing
+from typing import Union
+
 import redis
+from mysql.connector import MySQLConnection
+from mysql.connector.pooling import MySQLConnectionPool, PooledMySQLConnection
 
 from utils.singleton import SingletonMeta
 
 
-class RedisConnection(metaclass=SingletonMeta):
+class RedisConnectionPool(metaclass=SingletonMeta):
 
     def __init__(self, host='127.0.0.1', port=6379, socket_timeout=10) -> None:
-        self.host = host
-        self.port = port
-        self.conn = redis.Redis(host, port, socket_timeout=socket_timeout, decode_responses=True)
+        self.pool = redis.ConnectionPool(host=host, port=port, socket_timeout=socket_timeout, decode_responses=True)
+
+    def get_connection(self) -> redis.Redis:
+        return redis.Redis(connection_pool=self.pool)
 
 
-class MysqlConnection:
+class MysqlConnectionPool(metaclass=SingletonMeta):
 
     def __init__(self, host='127.0.0.1', port=3306) -> None:
-        self.host = host
-        self.port = port
-        self.conn = mysql.connector.connect(pool_size='cs6200', database='cs6200', user='cs6200', password='cs6200',
-                                            host=host, port=port)
+        self.pool = MySQLConnectionPool(pool_name='cs6200', database='cs6200', user='cs6200', password='cs6200',
+                                        host=host, port=port)
+
+    def get_connection(self) -> Union[PooledMySQLConnection, MySQLConnection]:
+        return self.pool.get_connection()
 
 
 class ConnectionFactory:
 
     @classmethod
-    def create_redis_connection(cls) -> RedisConnection:
-        return RedisConnection()
+    def create_redis_connection(cls) -> redis.Redis:
+        return RedisConnectionPool().get_connection()
 
     @classmethod
-    def create_mysql_connection(cls) -> MysqlConnection:
-        return MysqlConnection()
+    def create_mysql_connection(cls):
+        return closing(MysqlConnectionPool().get_connection())
