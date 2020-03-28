2e72684cb6b54923e61e3d8deb5ffe5b3565ca35
gajjar.s <gajjar.s@husky.neu.edu>
2020-03-28 16:17:33 -0400

    added code to query es and dump the results into file for hw3 queries

--- HW_5/main.py
+++ HW_5/main.py
@@ -1,16 +1,45 @@
+from HW_1.es_utils import EsUtils
+from constants.constants import Constants
 from utils.utils import Utils
 
 
 class HW5:
+    _QUERIES = [{'id': 152101, 'query': 'founding fathers'},
+                {'id': 152102, 'query': 'independence war causes'},
+                {'id': 152103, 'query': 'declaration of independence'}]
 
     @classmethod
-    def plot_precision_recall_plot(cls):
-        # TODO implement this
-        pass
+    def _query_vertical_search(cls, query):
+        es_client = EsUtils.get_es_client()
+        response = es_client.search(index=Constants.CRAWLED_DATA_INDEX_NAME, body={
+            "query": {
+                "query_string": {
+                    "query": query['query']
+                }
+            },
+            "size": 1000,
+            "stored_fields": []
+        })
+        results = []
+        for ix, doc in enumerate(response['hits']['hits']):
+            results.append({
+                'doc_no': doc['_id'],
+                'rank': ix + 1,
+                'score': doc['_score'],
+                'query_number': query['id']
+            })
+        return results
 
     @classmethod
     def main(cls):
-        pass
+        results_to_write = []
+        for query in cls._QUERIES:
+            results = cls._query_vertical_search(query)
+            results.sort(reverse=True, key=lambda d: d['score'])
+            results_to_write.extend(results)
+
+        file_path = 'results/hw_4_queries_result.txt'
+        Utils.write_results_to_file(file_path, results_to_write)
 
 
 if __name__ == '__main__':
