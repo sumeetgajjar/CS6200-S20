ec34335363ff054dfcf135daf2a51d3bebafc6c9
gajjar.s <gajjar.s@husky.neu.edu>
2020-03-28 14:23:51 -0400

    1. bug fix while calculating the r precision

--- HW_5/treq_eval.py
+++ HW_5/treq_eval.py
@@ -61,31 +61,33 @@ class TREQEval:
         result_str += "    Relevant:     {}\n".format(rel)
         result_str += "    Rel_ret:      {}\n".format(rel_ret)
         result_str += "Interpolated Recall - Precision Averages:\n"
-        result_str += "    at 0.00       %.4f\n".format(prec_at_recalls[0])
-        result_str += "    at 0.10       %.4f\n".format(prec_at_recalls[1])
-        result_str += "    at 0.20       %.4f\n".format(prec_at_recalls[2])
-        result_str += "    at 0.30       %.4f\n".format(prec_at_recalls[3])
-        result_str += "    at 0.40       %.4f\n".format(prec_at_recalls[4])
-        result_str += "    at 0.50       %.4f\n".format(prec_at_recalls[5])
-        result_str += "    at 0.60       %.4f\n".format(prec_at_recalls[6])
-        result_str += "    at 0.70       %.4f\n".format(prec_at_recalls[7])
-        result_str += "    at 0.80       %.4f\n".format(prec_at_recalls[8])
-        result_str += "    at 0.90       %.4f\n".format(prec_at_recalls[9])
-        result_str += "    at 1.00       %.4f\n".format(prec_at_recalls[10])
+        result_str += "    at 0.00       {0:.4f}\n".format(prec_at_recalls[0])
+        result_str += "    at 0.10       {0:.4f}\n".format(prec_at_recalls[1])
+        result_str += "    at 0.20       {0:.4f}\n".format(prec_at_recalls[2])
+        result_str += "    at 0.30       {0:.4f}\n".format(prec_at_recalls[3])
+        result_str += "    at 0.40       {0:.4f}\n".format(prec_at_recalls[4])
+        result_str += "    at 0.50       {0:.4f}\n".format(prec_at_recalls[5])
+        result_str += "    at 0.60       {0:.4f}\n".format(prec_at_recalls[6])
+        result_str += "    at 0.70       {0:.4f}\n".format(prec_at_recalls[7])
+        result_str += "    at 0.80       {0:.4f}\n".format(prec_at_recalls[8])
+        result_str += "    at 0.90       {0:.4f}\n".format(prec_at_recalls[9])
+        result_str += "    at 1.00       {0:.4f}\n".format(prec_at_recalls[10])
         result_str += "Average precision (non-interpolated) for all rel docs(averaged over queries)\n"
-        result_str += "                  %.4f\n".format(mean_avg_precision)
+        result_str += "                  {0:.4f}\n".format(mean_avg_precision)
         result_str += "Precision:\n"
-        result_str += "  At    5 docs:   %.4f\n".format(prec_at_cutoffs[0])
-        result_str += "  At   10 docs:   %.4f\n".format(prec_at_cutoffs[1])
-        result_str += "  At   15 docs:   %.4f\n".format(prec_at_cutoffs[2])
-        result_str += "  At   20 docs:   %.4f\n".format(prec_at_cutoffs[3])
-        result_str += "  At   30 docs:   %.4f\n".format(prec_at_cutoffs[4])
-        result_str += "  At  100 docs:   %.4f\n".format(prec_at_cutoffs[5])
-        result_str += "  At  200 docs:   %.4f\n".format(prec_at_cutoffs[6])
-        result_str += "  At  500 docs:   %.4f\n".format(prec_at_cutoffs[7])
-        result_str += "  At 1000 docs:   %.4f\n".format(prec_at_cutoffs[8])
+        result_str += "  At    5 docs:   {0:.4f}\n".format(prec_at_cutoffs[0])
+        result_str += "  At   10 docs:   {0:.4f}\n".format(prec_at_cutoffs[1])
+        result_str += "  At   15 docs:   {0:.4f}\n".format(prec_at_cutoffs[2])
+        result_str += "  At   20 docs:   {0:.4f}\n".format(prec_at_cutoffs[3])
+        result_str += "  At   30 docs:   {0:.4f}\n".format(prec_at_cutoffs[4])
+        result_str += "  At  100 docs:   {0:.4f}\n".format(prec_at_cutoffs[5])
+        result_str += "  At  200 docs:   {0:.4f}\n".format(prec_at_cutoffs[6])
+        result_str += "  At  500 docs:   {0:.4f}\n".format(prec_at_cutoffs[7])
+        result_str += "  At 1000 docs:   {0:.4f}\n".format(prec_at_cutoffs[8])
         result_str += "R-Precision (precision after R (= num_rel for a query) docs retrieved):\n"
-        result_str += "    Exact:        %.4f\n".format(r_prec)
+        result_str += "    Exact:        {:.4}\n".format(r_prec)
+
+        logging.info(result_str)
 
     def eval(self):
         qrel, num_relevance = self._parse_qrel_file()
@@ -94,10 +96,10 @@ class TREQEval:
         tot_num_ret = 0
         tot_num_rel = 0
         tot_num_rel_ret = 0
-        sum_prec_at_cutoffs = {}
-        sum_prec_at_recalls = {}
-        avg_prec_at_cutoffs = {}
-        avg_prec_at_recalls = {}
+        sum_prec_at_cutoffs = defaultdict(float)
+        sum_prec_at_recalls = defaultdict(float)
+        avg_prec_at_cutoffs = defaultdict(float)
+        avg_prec_at_recalls = defaultdict(float)
         sum_avg_prec = 0
         sum_r_prec = 0
 
@@ -144,8 +146,8 @@ class TREQEval:
                 int_num_rel = int(num_relevance[query_id])
                 frac_num_rel = num_relevance[query_id] - int_num_rel
 
-                r_prec = (1 - frac_num_rel) * prec_list[int_num_rel] + frac_num_rel * prec_list[
-                    int_num_rel + 1] if frac_num_rel > 0 else prec_list[int_num_rel]
+                r_prec = (1 - frac_num_rel) * prec_list[int_num_rel - 1] + frac_num_rel * prec_list[
+                    int_num_rel - 1] if frac_num_rel > 0 else prec_list[int_num_rel - 1]
 
             max_prec = 0
             for ix in range(self._MAX_DOCS_TO_CONSIDER - 1, -1, -1):
@@ -187,7 +189,7 @@ class TREQEval:
             avg_prec_at_cutoffs[ix] = sum_prec_at_cutoffs[ix] / num_topics
 
         for ix in range(len(self._RECALLS)):
-            avg_prec_at_cutoffs[ix] = sum_prec_at_recalls[ix] / num_topics
+            avg_prec_at_recalls[ix] = sum_prec_at_recalls[ix] / num_topics
 
         mean_avg_prec = sum_avg_prec / num_topics
         avg_r_prec = sum_r_prec / num_topics
@@ -198,10 +200,10 @@ class TREQEval:
 
 if __name__ == '__main__':
     Utils.configure_logging()
-    if len(sys.argv) < 2 or len(sys.argv) > 3:
+    if len(sys.argv) < 3 or len(sys.argv) > 4:
         logging.error("Usage: treq_eval.py [-q] qrel_file_path treq_file_path")
         sys.exit(1)
 
-    print_all_queries = len(sys.argv) == 3
+    print_all_queries = len(sys.argv) == 4
     treq_eval = TREQEval(sys.argv[-2], sys.argv[-1], print_all_queries)
     treq_eval.eval()
