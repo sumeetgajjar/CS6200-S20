ff0998d69f52a213bd2f71d6ebefc985e4a0250f
gajjar.s <gajjar.s@husky.neu.edu>
2020-04-08 00:08:52 -0400

    1. added completed the pipeline for prediction

--- HW_7/main.py
+++ HW_7/main.py
@@ -13,9 +13,13 @@ from nltk import SnowballStemmer
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from scipy.sparse import csr_matrix
-from sklearn.datasets import dump_svmlight_file
+from sklearn.datasets import dump_svmlight_file, load_svmlight_file
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import roc_auc_score
 from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import BernoulliNB
+from sklearn.tree import DecisionTreeClassifier
 
 from utils.decorators import timing
 from utils.utils import Utils
@@ -41,6 +45,8 @@ class Email:
 class HW7:
     _SPAM_EMAIL_DATA_DIR_PATH = '{}/SPAM_DATA/trec07p/data'.format(Utils.get_data_dir_abs_path(), )
     _SPAM_EMAIL_LABELS_PATH = '{}/SPAM_DATA/trec07p/full/index'.format(Utils.get_data_dir_abs_path())
+    _CACHED_FEATURE_INDEX_NAME = 'feature_matrix_cache/feature_index.json'
+    _CACHED_FEATURES_FILE_PATH = 'feature_matrix_cache/features.txt'
     _SPLIT_REGEX = re.compile("\\s+")
     _PUNCTUATION_TABLE = str.maketrans('', '', string.punctuation)
     _STOPWORDS_SET = set(stopwords.words('english'))
@@ -148,37 +154,68 @@ class HW7:
 
     @classmethod
     @timing
-    def _generate_features(cls, labels_dict):
+    def _generate_features(cls, use_cached=True):
+        if use_cached:
+            X, y = load_svmlight_file(cls._CACHED_FEATURES_FILE_PATH)
+            with open(cls._CACHED_FEATURE_INDEX_NAME, 'r') as file:
+                feature_name_index = json.load(file)
+        else:
+            labels_dict = cls._parse_labels()
+            all_email_files = os.listdir(cls._SPAM_EMAIL_DATA_DIR_PATH)
+            results = Utils.run_tasks_parallelly_in_chunks(cls._get_email_contents_and_labels, all_email_files, 12,
+                                                           labels_dict=labels_dict)
 
-        all_email_files = os.listdir(cls._SPAM_EMAIL_DATA_DIR_PATH)
-        results = Utils.run_tasks_parallelly_in_chunks(cls._get_email_contents_and_labels, all_email_files, 12,
-                                                       labels_dict=labels_dict)
+            corpus = []
+            all_labels = []
+            for email_contents, labels in results:
+                corpus.extend(email_contents)
+                all_labels.extend(labels)
 
-        corpus = []
-        all_labels = []
-        for email_contents, labels in results:
-            corpus.extend(email_contents)
-            all_labels.extend(labels)
+            vectorizer = CountVectorizer(ngram_range=(1, 1), min_df=0.02, max_df=0.95)
+            X = vectorizer.fit_transform(corpus)
+            #
+            rows = list(range(len(all_labels)))
+            cols = [0] * len(all_labels)
+            y = csr_matrix((all_labels, (rows, cols)), shape=(len(rows), 1))
 
-        vectorizer = CountVectorizer(ngram_range=(1, 1), min_df=0.02, max_df=0.95)
-        X = vectorizer.fit_transform(corpus)
-        #
-        rows = list(range(len(all_labels)))
-        cols = [0] * len(all_labels)
-        y = csr_matrix((all_labels, (rows, cols)), shape=(len(rows), 1))
-
-        feature_index = vectorizer.get_feature_names()
-        dump_svmlight_file(X, y, f='feature_matrix_cache/features.txt')
-        with open('feature_matrix_cache/feature_index.json', 'w') as file:
-            json.dump(feature_index, file)
+            feature_name_index = vectorizer.get_feature_names()
+            dump_svmlight_file(X, y, f=cls._CACHED_FEATURES_FILE_PATH)
+            with open(cls._CACHED_FEATURE_INDEX_NAME, 'w') as file:
+                json.dump(feature_name_index, file)
 
         X_train, X_test, Y_train, Y_test = train_test_split(X, y, test_size=0.2)
-        return X_train, X_test, Y_train, Y_test, feature_index
+        return X_train, X_test, Y_train, Y_test, feature_name_index
+
+    @classmethod
+    @timing
+    def _run_model(cls, model, model_name, X_train, X_test, Y_train, Y_test, feature_name_index):
+        logging.info("Running {}".format(model_name))
+        model.fit(X_train, Y_train)
+
+        def _run_prediction_phase(phase_name, X, Y_true):
+            logging.info("Running {} predictions".format(phase_name))
+            Y_predict = model.predict(X)
+            Y_probs = model.predict_proba(X)[:, 1]
+            auc_score = roc_auc_score(Y_true, Y_probs)
+            logging.info("AUC score for {} for {} phase:{}".format(model_name, phase_name, auc_score))
+
+        logging.info("Feature importance")
+
+        _run_prediction_phase('training', X_train, Y_train)
+        _run_prediction_phase('testing', X_test, Y_test)
 
     @classmethod
     def main(cls):
-        labels_dict = cls._parse_labels()
-        X_train, X_test, Y_train, Y_test, feature_index = cls._generate_features(labels_dict)
+        X_train, X_test, Y_train, Y_test, feature_name_index = cls._generate_features()
+        for model, model_name in [
+            (LogisticRegression(solver='newton-cg', fit_intercept=True), "LogisticRegression"),
+            (DecisionTreeClassifier(max_depth=5), "DecisionTree-5"),
+            (DecisionTreeClassifier(max_depth=10), "DecisionTree-10"),
+            (DecisionTreeClassifier(max_depth=15), "DecisionTree-15"),
+            (DecisionTreeClassifier(max_depth=20), "DecisionTree-20"),
+            (BernoulliNB(), "BernoulliNB")
+        ]:
+            cls._run_model(model, model_name, X_train, X_test, Y_train, Y_test, feature_name_index)
 
 
 if __name__ == '__main__':
